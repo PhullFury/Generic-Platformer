eb626e6b7fbd31d439d2ca299bebf3719eb31c8b
PhullFury <66832169+PhullFury@users.noreply.github.com>
2021-07-30 00:18:37 +0530

    Moving platforms added.

--- Source/Platformer/Components/CustomMovComp.cpp
+++ Source/Platformer/Components/CustomMovComp.cpp
@@ -6,29 +6,153 @@
 // Sets default values for this component's properties
 UCustomMovComp::UCustomMovComp()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
+	
 }
-
 
 // Called when the game starts
 void UCustomMovComp::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	OwnerLocation = GetOwner()->GetActorLocation();
+	CurrentX = OwnerLocation.X;
+	CurrentY = OwnerLocation.Y;
+	CurrentZ = OwnerLocation.Z;
+	bCanMove = true;
+	bIsTurn = true;
 }
-
 
 // Called every frame
 void UCustomMovComp::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	if (GetWorld()->GetTimeSeconds() - TimeSinceStop >= StopTime)
+	{
+		bCanMove = true;
+	}
+	Movement(DeltaTime);
 }
 
+void UCustomMovComp::Movement(float DeltaTime)
+{
+	if (bCanMove)
+	{
+		if (bPositiveMove)
+		{
+			if (bIsTurn)
+			{
+				PositiveMovement(DeltaTime);
+			}
+			else if (!bIsTurn)
+			{
+				NegativeMovement(DeltaTime);
+			}
+		}
+		else if (!bPositiveMove)
+		{
+			if (bIsTurn)
+			{
+				NegativeMovement(DeltaTime);
+			}
+			else if (!bIsTurn)
+			{
+				PositiveMovement(DeltaTime);
+			}
+		}
+	}
+}
+
+void UCustomMovComp::PositiveMovement(float DeltaTime)
+{
+	FVector NewOwnerLocation = GetOwner()->GetActorLocation();
+	if (bMoveX)
+	{
+		float EndX = OwnerLocation.X + MoveDist;
+		CurrentX = FMath::FInterpTo(CurrentX, EndX, DeltaTime, MoveSpeed);
+		if (CurrentX >= EndX - MinDist)
+		{
+			CurrentX = EndX;
+			HandleMoveEnd();
+		}
+		NewOwnerLocation.X = CurrentX;
+	}
+	else if (bMoveY)
+	{
+		float EndY = OwnerLocation.Y + MoveDist;
+		CurrentY = FMath::FInterpTo(CurrentY, EndY, DeltaTime, MoveSpeed);
+		if (CurrentY >= EndY - MinDist)
+		{
+			CurrentY = EndY;
+			HandleMoveEnd();
+		}
+		NewOwnerLocation.Y = CurrentY;
+	}
+	else if (bMoveZ)
+	{
+		float EndZ = OwnerLocation.Z + MoveDist;
+		CurrentZ = FMath::FInterpTo(CurrentZ, EndZ, DeltaTime, MoveSpeed);
+		if (CurrentZ >= EndZ - MinDist)
+		{
+			CurrentZ = EndZ;
+			HandleMoveEnd();
+		}
+		NewOwnerLocation.Z = CurrentZ;
+	}
+	GetOwner()->SetActorLocation(NewOwnerLocation);
+}
+
+void UCustomMovComp::NegativeMovement(float DeltaTime)
+{
+	FVector NewOwnerLocation = GetOwner()->GetActorLocation();
+	if (bMoveX)
+	{
+		float EndX = OwnerLocation.X - MoveDist;
+		CurrentX = FMath::FInterpTo(CurrentX, EndX, DeltaTime, MoveSpeed);
+		if (CurrentX <= EndX + MinDist)
+		{
+			CurrentX = EndX;
+			HandleMoveEnd();
+		}
+		NewOwnerLocation.X = CurrentX;
+	}
+	else if (bMoveY)
+	{
+		float EndY = OwnerLocation.Y - MoveDist;
+		CurrentY = FMath::FInterpTo(CurrentY, EndY, DeltaTime, MoveSpeed);
+		if (CurrentY <= EndY + MinDist)
+		{
+			CurrentY = EndY;
+			HandleMoveEnd();
+		}
+		NewOwnerLocation.Y = CurrentY;
+	}
+	else if (bMoveZ)
+	{
+		float EndZ = OwnerLocation.Z - MoveDist;
+		CurrentZ = FMath::FInterpTo(CurrentZ, EndZ, DeltaTime, MoveSpeed);
+		if (CurrentZ <= EndZ + MinDist)
+		{
+			CurrentZ = EndZ;
+			HandleMoveEnd();
+		}
+		NewOwnerLocation.Z = CurrentZ;
+	}
+	GetOwner()->SetActorLocation(NewOwnerLocation);
+}
+
+void UCustomMovComp::HandleMoveEnd()
+{
+	TimeSinceStop = GetWorld()->GetTimeSeconds();
+	OwnerLocation = GetOwner()->GetActorLocation();
+	bCanMove = false;
+	if (bIsTurn)
+	{
+		bIsTurn = false;
+	}
+	else if (!bIsTurn)
+	{
+		bIsTurn = true;
+	}
+}
--- Source/Platformer/Components/CustomMovComp.h
+++ Source/Platformer/Components/CustomMovComp.h
@@ -15,14 +15,41 @@ class PLATFORMER_API UCustomMovComp : public UActorComponent
 public:	
 	// Sets default values for this component's properties
 	UCustomMovComp();
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+private:
+	UPROPERTY(EditAnywhere, Category = "Movement")
+		float StopTime = 2.f;
+	UPROPERTY(EditAnywhere, Category = "Movement")
+		float MoveDist = 200.f;
+	UPROPERTY(EditAnywhere, Category = "Movement")
+		float MoveSpeed = 2.f;
+	UPROPERTY(EditAnywhere, Category = "Movement")
+		float MinDist = 5.f;
+	UPROPERTY(EditAnywhere, Category = "Movement")
+		bool bMoveX = true;
+	UPROPERTY(EditAnywhere, Category = "Movement")
+		bool bMoveY = false;
+	UPROPERTY(EditAnywhere, Category = "Movement")
+		bool bMoveZ = false;
+	UPROPERTY(EditAnywhere, Category = "Movement")
+		bool bPositiveMove = true;
 
-		
+	FVector OwnerLocation;
+	bool bCanMove;
+	bool bIsTurn;
+	float TimeSinceStop;
+	float CurrentX;
+	float CurrentY;
+	float CurrentZ;
+
+	void HandleMoveEnd();
+	void Movement(float DeltaTime);
+	void PositiveMovement(float DeltaTime);
+	void NegativeMovement(float DeltaTime);
 };
