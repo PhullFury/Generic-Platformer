0a35009be1d4faba6cafc86c2280b71c3ff23e75
PhullFury <66832169+PhullFury@users.noreply.github.com>
2021-08-29 22:34:23 +0530

    The turret works. Next to make the projectile do stuff.

---
+++ Source/Platformer/Actors/ProjectileBase.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ProjectileBase.h"
+#include "DrawDebugHelpers.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+// Sets default values
+AProjectileBase::AProjectileBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Static Mesh"));
+	RootComponent = Mesh;
+}
+
+// Called when the game starts or when spawned
+void AProjectileBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AProjectileBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
---
+++ Source/Platformer/Actors/TurretBase.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TurretBase.h"
+#include "Components/CapsuleComponent.h"
+#include "Platformer/Actors/ProjectileBase.h"
+
+// Sets default values
+ATurretBase::ATurretBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Capsule = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Collision"));
+	RootComponent = Capsule;
+
+	Mesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Static Mesh"));
+	Mesh->SetupAttachment(RootComponent);
+
+	SpawnPoint = CreateDefaultSubobject<USceneComponent>(TEXT("Spawn Point"));
+	SpawnPoint->SetupAttachment(Mesh);
+}
+
+// Called when the game starts or when spawned
+void ATurretBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	bCanShoot = true;
+	TimeSinceShoot = GetWorld()->GetTimeSeconds();
+}
+
+// Called every frame
+void ATurretBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (GetWorld()->GetTimeSeconds() - TimeSinceShoot >= ShootTimer)
+	{
+		Shoot();
+		//bCanShoot = true;
+	}
+
+	/*if (bCanShoot)
+	{
+		Shoot();
+		bCanShoot = false;
+		TimeSinceShoot = GetWorld()->GetTimeSeconds();
+	}*/
+}
+
+void ATurretBase::Shoot()
+{
+	AProjectileBase* Bullet = GetWorld()->SpawnActor<AProjectileBase>(Projectile, GetActorLocation(), GetActorRotation());
+	Bullet->SetOwner(this);
+	TimeSinceShoot = GetWorld()->GetTimeSeconds();
+	
+}
